1c2e31c03764ae95d4f5c8e8f3138b9398ff8cec
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2022-10-12 09:30:20 +0300

    add js-close-menu-anchore

---
+++ src/js/menu-close-anchor.js
@@ -0,0 +1,25 @@
+$('.js-anchor-scroll').click(function () {
+  $('.js-mobile-menu').removeClass('is-open');
+});
+
+$(document).ready(function () {
+  $('a').on('click', function (event) {
+    if (this.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      // Store hash
+      var hash = this.hash;
+
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
