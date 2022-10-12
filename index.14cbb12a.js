a8c3163735bd4480ffa5a809a5e63020bb7a4170
alexxxusachev888 <101808864+alexxxusachev888@users.noreply.github.com>
2022-10-12 22:57:41 +0000

    Deploying to gh-pages from @ alexxxusachev888/ice-cream-shop@ea244093b780e5ba42978a7b4ab4b3a3d507c481 🚀

--- index.14cbb12a.js
+++
@@ -1,2 +0,0 @@
-window.onscroll=function(){scrollY>=1650&&document.querySelector(".cow-drop").classList.add("drop")};
-//# sourceMappingURL=index.14cbb12a.js.map
---
+++ index.198d3590.js
@@ -0,0 +1,2 @@
+window.onscroll=function(){scrollY>=1650&&document.querySelector(".cow-drop").classList.add("drop"),window.screen.width>=768?scrollY<=70?document.getElementById("button-up").classList.remove("button-up__scroll"):document.getElementById("button-up").classList.add("button-up__scroll"):scrollY<=120?document.getElementById("button-up").classList.remove("button-up__scroll"):document.getElementById("button-up").classList.add("button-up__scroll")};
+//# sourceMappingURL=index.198d3590.js.map
