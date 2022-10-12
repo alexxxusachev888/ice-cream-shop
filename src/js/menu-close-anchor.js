a6ec0a740e043f7fd22569db1c88eeb03cfa5bfb
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2022-10-12 09:41:12 +0300

    Update menu-close-anchor.js

--- src/js/menu-close-anchor.js
+++ src/js/menu-close-anchor.js
@@ -8,16 +8,6 @@ $(document).ready(function () {
       event.preventDefault();
 
       var hash = this.hash;
-
-      $('html, body').animate(
-        {
-          scrollTop: $(hash).offset().top,
-        },
-        800,
-        function () {
-          window.location.hash = hash;
-        }
-      );
     }
   });
 });
