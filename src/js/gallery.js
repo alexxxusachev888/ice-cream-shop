cf02865a1ee850a383768600759ebd07703c8cb7
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-10-12 04:17:57 +0300

    add-slick

---
+++ src/js/gallery.js
@@ -0,0 +1,11 @@
+$('.gallery__list').slick({
+  dots: false,
+  arrows: false,
+  speed: 300,
+  fade: true,
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 5000,
+  pauseOnFocus: true,
+  pauseOnHover: true,
+});
