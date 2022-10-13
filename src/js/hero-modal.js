9d5af00ce3e8b0744e38f58bb3d6214eda729d87
alexxxusachev888 <alexxxusachev888@gmail.com>
2022-10-13 22:46:20 +0300

    mobile-btn

--- src/js/hero-modal.js
+++ src/js/hero-modal.js
@@ -10,3 +10,18 @@
     modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtnMobile: document.getElementById('buy-now-btn'),
+    modal: document.querySelector('[data-hero-modal]'),
+    menuMob: document.querySelector('.js-menu-container')
+  };
+
+  refs.openModalBtnMobile.addEventListener('click', openModal);
+
+  function openModal() {
+    refs.modal.classList.remove('is-hidden');
+    refs.menuMob.classList.remove('is-open');
+  }
+})();
